f995ff5d4674843658256c04aece97b4b9b291f3
sleepanda4523 <63851752+sleepanda4523@users.noreply.github.com>
2021-06-08 23:09:33 +0900

    3강

--- basic/index.js
+++ basic/index.js
@@ -2,6 +2,13 @@ const express =  require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://sleepanda:4523@nodereact1.faoop.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connect'))
+    .catch(err => console.log(err))
+
+
 app.get('/', (req,res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
