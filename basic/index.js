54caf4e2124ceabef8cd93d0f443123f339be173
sleepanda4523 <63851752+sleepanda4523@users.noreply.github.com>
2021-06-15 22:10:26 +0900

    9강

--- basic/index.js
+++ basic/index.js
@@ -25,6 +25,7 @@ app.post('/register', (req, res) => {
     //회원가입 할때 필요한 정보들을 Client에서 가져오면
     //그것들을 DB에 넣어줌.
     const user = new User(req.body);
+    
     user.save((err, userInfo) => {
         if(err) return res.json({success: false, err})
         return res.status(200).json({
