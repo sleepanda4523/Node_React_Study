adafbf8d9c68cf7e026887853844220fd9ad30cc
sleepanda4523 <63851752+sleepanda4523@users.noreply.github.com>
2021-07-06 23:26:13 +0900

    redux

---
+++ basic/client/src/_reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+//import user from './user_reducer';
+
+// Reducer를 합치는 기능
+const rootReducer = combineReducers({
+    // user
+})
+
+
+export default rootReducer;
